23dd6e1f293f1a03f07d99f72a6e14c7f1454917
ZORAN RISTOVSKI <zoranrist@yahoo.com>
2020-09-02 19:44:23 +0200

    promenlivi_funkcii_izrazi_stejtmenti

--- js/hello.js
+++ js/hello.js
@@ -1,6 +1,52 @@
-let hello = "Hello World";
+// Variable declaration
+
+let hello;
+
+hello = "Hello World";
+hello = "Hello World 2020";
+
+15 + 15; // 30
+
+"Hello" + "World"; // "Hello World"
+
+let bolleanVariable = true;
+let bolleanVariableF = false;
+
+result = 0;
+
+// Function declaration statement
+
+function zbir(a, b) {
+  result = a + b;
+}
+
+zbir(2, 2);
+
+console.log("Rezultatot e: " + result);
+
+// {variable o will have similar structure to this one:
+
+// {
+//     a: 1,
+//     b: 2,
+//     hello: "Hello World" ...
+// }
+
+let o = new Object();
+
+// variable a will have similar structure to this one:
+
+// niza sekogash zapochniva od 0 index
+
+// ["1", 5, true, "Hello World"]
+
+let a = new Array();
 
 let h1_hello = document.querySelector("h1");
 
+/* find the h1 tag inside index.html */
 // h1_hello.textContent = "Zoran Ristovski";
 h1_hello.textContent = hello;
+
+hello = null;
+let number = 20;
